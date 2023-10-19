46a3179048eba9167b042eac1340076f44d5ca07
Li Bin <lbin@outlook.com>
2023-10-19 18:27:25 +0800

    convert coorp: veh part

--- configs/vic3d/early-fusion-pointcloud/pointpillars/trainval_config_dair.py
+++ configs/vic3d/early-fusion-pointcloud/pointpillars/trainval_config_dair.py
@@ -1,22 +1,101 @@
 _base_ = ['../../../__base__/schedules/cyclic-40e.py', '../../../__base__/default_runtime.py']
 dataset_type = 'KittiDataset'
-point_cloud_range = [0, -39.68, -3, 92.16, 39.68, 1]
-voxel_size = [0.16, 0.16, 4]
-
-data_root = './data/DAIR-V2X/cooperative-vehicle-infrastructure/vehicle-side/'
+data_root = './data/DAIR-V2X/cooperative-vehicle-infrastructure/'
 class_names = ['Car']
+# point_cloud_range = [0, -39.68, -3, 92.16, 39.68, 1]
+point_cloud_range = [0, -46.08, -3, 92.16, 46.08, 1]
+voxel_size = [0.16, 0.16, 4]
+length = int((point_cloud_range[3] - point_cloud_range[0]) / voxel_size[0])
+height = int((point_cloud_range[4] - point_cloud_range[1]) / voxel_size[1])
+output_shape = [height, length]
+# z_center_car = -1.78
+z_center_pedestrian = -0.6
+z_center_cyclist = -0.6
+z_center_car = -2.66
+
 metainfo = dict(classes=class_names)
 input_modality = dict(use_lidar=True, use_camera=False)
 
-z_center_pedestrian = -0.6
-z_center_cyclist = -0.6
-z_center_car = -1.78
+work_dir = './work_dirs/vic3d_earlyfusion_pointpillars'
+
+model = dict(
+    type='VoxelNet',
+    data_preprocessor=dict(
+        type='Det3DDataPreprocessor',
+        voxel=True,
+        voxel_layer=dict(
+            max_num_points=100,  # max_points_per_voxel
+            point_cloud_range=point_cloud_range,
+            voxel_size=voxel_size,
+            max_voxels=(16000, 40000))),
+    voxel_encoder=dict(
+        type='PillarFeatureNet',
+        in_channels=4,
+        feat_channels=[64],
+        with_distance=False,
+        voxel_size=voxel_size,
+        point_cloud_range=point_cloud_range,
+    ),
+    middle_encoder=dict(type='PointPillarsScatter', in_channels=64, output_shape=output_shape),
+    backbone=dict(type='SECOND', in_channels=64, layer_nums=[3, 5, 5], layer_strides=[2, 2, 2], out_channels=[64, 128, 256]),
+    neck=dict(type='SECONDFPN', in_channels=[64, 128, 256], upsample_strides=[1, 2, 4], out_channels=[128, 128, 128]),
+    bbox_head=dict(
+        type='Anchor3DHead',
+        num_classes=1,
+        in_channels=384,
+        feat_channels=384,
+        use_direction_classifier=True,
+        anchor_generator=dict(
+            type='Anchor3DRangeGenerator',
+            ranges=[
+                [
+                    point_cloud_range[0],
+                    point_cloud_range[1],
+                    z_center_car,
+                    point_cloud_range[3],
+                    point_cloud_range[4],
+                    z_center_car,
+                ],
+            ],
+            sizes=[[3.9, 1.6, 1.56]],
+            rotations=[0, 1.57],
+            reshape_out=False,
+        ),
+        diff_rad_by_sin=True,
+        bbox_coder=dict(type='DeltaXYZWLHRBBoxCoder'),
+        loss_cls=dict(type='mmdet.FocalLoss', use_sigmoid=True, gamma=2.0, alpha=0.25, loss_weight=1.0),
+        loss_bbox=dict(type='mmdet.SmoothL1Loss', beta=1.0 / 9.0, loss_weight=2.0),
+        loss_dir=dict(type='mmdet.CrossEntropyLoss', use_sigmoid=False, loss_weight=0.2)),
+    train_cfg=dict(
+        assigner=[
+            dict(
+                type='Max3DIoUAssigner',
+                iou_calculator=dict(type='mmdet3d.BboxOverlapsNearest3D'),
+                pos_iou_thr=0.6,
+                neg_iou_thr=0.45,
+                min_pos_iou=0.45,
+                ignore_iof_thr=-1,
+            ),
+        ],
+        allowed_border=0,
+        pos_weight=-1,
+        debug=False,
+    ),
+    test_cfg=dict(
+        use_rotate_nms=False,
+        nms_across_levels=False,
+        nms_thr=0.01,
+        score_thr=0.2,
+        min_bbox_size=0,
+        nms_pre=1000,
+        max_num=300,
+    ),
+)
 
 backend_args = None
-
 db_sampler = dict(
     data_root=data_root,
-    info_path=data_root + 'kitti_dbinfos_train.pkl',
+    info_path=data_root + 'dair_dbinfos_train.pkl',
     rate=1.0,
     prepare=dict(filter_by_difficulty=[-1], filter_by_min_points=dict(Car=5)),
     classes=class_names,
@@ -24,6 +103,7 @@ db_sampler = dict(
     points_loader=dict(type='LoadPointsFromFile', coord_type='LIDAR', load_dim=4, use_dim=4, backend_args=backend_args),
     backend_args=backend_args)
 
+file_client_args = dict(backend='disk')
 train_pipeline = [
     dict(type='LoadPointsFromFile', coord_type='LIDAR', load_dim=4, use_dim=4, backend_args=backend_args),
     dict(type='LoadAnnotations3D', with_bbox_3d=True, with_label_3d=True),
@@ -63,7 +143,7 @@ train_dataloader = dict(
             type=dataset_type,
             data_root=data_root,
             ann_file='dair_infos_train.pkl',
-            data_prefix=dict(pts='training/velodyne_reduced'),
+            data_prefix=dict(pts=''),
             pipeline=train_pipeline,
             modality=input_modality,
             test_mode=False,
@@ -81,7 +161,7 @@ val_dataloader = dict(
     dataset=dict(
         type=dataset_type,
         data_root=data_root,
-        data_prefix=dict(pts='training/velodyne_reduced'),
+        data_prefix=dict(pts=''),
         ann_file='dair_infos_val.pkl',
         pipeline=test_pipeline,
         modality=input_modality,
@@ -98,7 +178,7 @@ test_dataloader = dict(
     dataset=dict(
         type=dataset_type,
         data_root=data_root,
-        data_prefix=dict(pts='training/velodyne_reduced'),
+        data_prefix=dict(pts=''),
         ann_file='dair_infos_val.pkl',
         pipeline=test_pipeline,
         modality=input_modality,
@@ -108,8 +188,9 @@ test_dataloader = dict(
         backend_args=backend_args))
 val_evaluator = dict(type='KittiMetric', ann_file=data_root + 'dair_infos_val.pkl', metric='bbox', backend_args=backend_args)
 test_evaluator = val_evaluator
+
 # optimizer
-lr = 0.001
+lr = 0.0018
 epoch_num = 80
 optim_wrapper = dict(optimizer=dict(lr=lr), clip_grad=dict(max_norm=35, norm_type=2))
 param_scheduler = [
@@ -119,52 +200,6 @@ param_scheduler = [
     dict(type='CosineAnnealingMomentum', T_max=epoch_num * 0.6, eta_min=1, begin=epoch_num * 0.4, end=epoch_num * 1, convert_to_iter_based=True)
 ]
 
-train_cfg = dict(by_epoch=True, max_epochs=epoch_num, val_interval=2)
+train_cfg = dict(by_epoch=True, max_epochs=epoch_num, val_interval=10)
 val_cfg = dict()
 test_cfg = dict()
-
-model = dict(
-    type='VoxelNet',
-    data_preprocessor=dict(
-        type='Det3DDataPreprocessor',
-        voxel=True,
-        voxel_layer=dict(
-            max_num_points=32,  # max_points_per_voxel
-            point_cloud_range=[0, -39.68, -3, 92.16, 39.68, 1],
-            voxel_size=voxel_size,
-            max_voxels=(16000, 40000))),
-    voxel_encoder=dict(type='PillarFeatureNet', in_channels=4, feat_channels=[64], with_distance=False, voxel_size=voxel_size, point_cloud_range=[0, -39.68, -3, 92.16, 39.68, 1]),
-    middle_encoder=dict(type='PointPillarsScatter', in_channels=64, output_shape=[496, 576]),
-    backbone=dict(type='SECOND', in_channels=64, layer_nums=[3, 5, 5], layer_strides=[2, 2, 2], out_channels=[64, 128, 256]),
-    neck=dict(type='SECONDFPN', in_channels=[64, 128, 256], upsample_strides=[1, 2, 4], out_channels=[128, 128, 128]),
-    bbox_head=dict(
-        type='Anchor3DHead',
-        num_classes=1,
-        in_channels=384,
-        feat_channels=384,
-        use_direction_classifier=True,
-        assign_per_class=True,
-        anchor_generator=dict(
-            type='AlignedAnchor3DRangeGenerator', ranges=[
-                [0, -39.68, -1.78, 92.16, 39.68, -1.78],
-            ], sizes=[[3.9, 1.6, 1.56]], rotations=[0, 1.57], reshape_out=False),
-        diff_rad_by_sin=True,
-        bbox_coder=dict(type='DeltaXYZWLHRBBoxCoder'),
-        loss_cls=dict(type='mmdet.FocalLoss', use_sigmoid=True, gamma=2.0, alpha=0.25, loss_weight=1.0),
-        loss_bbox=dict(type='mmdet.SmoothL1Loss', beta=1.0 / 9.0, loss_weight=2.0),
-        loss_dir=dict(type='mmdet.CrossEntropyLoss', use_sigmoid=False, loss_weight=0.2)),
-    # model training and testing settings
-    train_cfg=dict(
-        assigner=[
-            dict(  # for Car
-                type='Max3DIoUAssigner',
-                iou_calculator=dict(type='mmdet3d.BboxOverlapsNearest3D'),
-                pos_iou_thr=0.6,
-                neg_iou_thr=0.45,
-                min_pos_iou=0.45,
-                ignore_iof_thr=-1),
-        ],
-        allowed_border=0,
-        pos_weight=-1,
-        debug=False),
-    test_cfg=dict(use_rotate_nms=True, nms_across_levels=False, nms_thr=0.01, score_thr=0.1, min_bbox_size=0, nms_pre=100, max_num=50))
--- tools/convert_dair_c_to_mmdet3d.py
+++ tools/convert_dair_c_to_mmdet3d.py
@@ -2,9 +2,7 @@ import argparse
 import os
 
 import numpy as np
-from dataset_converters.gen_kitti.gen_calib2kitti import convert_calib_v2x_to_kitti, get_cam_D_and_cam_K, get_velo2cam
 from dataset_converters.gen_kitti.label_lidarcoord_to_cameracoord import convert_point, get_camera_3d_8points, get_label
-from dataset_converters.kitti_data_utils import _extend_matrix
 from dataset_converters.update_infos_to_v2 import get_empty_instance
 from mmdet3d.structures import points_cam2img
 from mmdet3d.structures.ops import box_np_ops
@@ -13,22 +11,18 @@ from skimage import io
 from tqdm import tqdm
 
 parser = argparse.ArgumentParser(description='Data converter arg parser')
-parser.add_argument('--dataset', type=str, default='vehicle-side')
+parser.add_argument('--dataset', type=str, default='cooperative')
 parser.add_argument('--root-path', type=str, default='./data/DAIR-V2X/cooperative-vehicle-infrastructure/', help='specify the root path of dataset')
-parser.add_argument('--split_file_path', type=str, default='data/split_datas/single-vehicle-split-data.json', help='specify the split file')
+parser.add_argument('--split_file_path', type=str, default='data/split_datas/cooperative-split-data.json', help='specify the split file')
 args = parser.parse_args()
 
 
 def get_split_list(split_path):
     ori_list = load(split_path)
-    print('Total number of samples: ', len(ori_list))
-    print('Number of training samples: ', len(ori_list['train']))
-    print('Number of validation samples: ', len(ori_list['val']))
-    print('Number of testing samples: ', len(ori_list['test']))
-    print(ori_list.keys())
+
     split_list = {}
-    split_list['train'] = ori_list['train']
-    split_list['val'] = ori_list['val']
+    split_list['train'] = ori_list['cooperative_split']['train']
+    split_list['val'] = ori_list['cooperative_split']['val']
     split_list['trainval'] = split_list['train'] + split_list['val']
     return split_list
 
@@ -43,7 +37,11 @@ def get_metainfo():
     return metainfo
 
 
-def get_images_info(ori_info, defalut_cam, root_path):
+def get_calib_info():
+    pass
+
+
+def get_images_info(ori_info, defalut_cam, root_path, veh_idx, inf_idx, sensor_view='vehicle'):
     images = {}
     for cam_idx in range(4):
         images['CAM%d' % cam_idx] = {}
@@ -53,41 +51,29 @@ def get_images_info(ori_info, defalut_cam, root_path):
         images['CAM%d' % cam_idx]['cam2img'] = []
         images['CAM%d' % cam_idx]['lidar2img'] = []
         images['CAM%d' % cam_idx]['lidar2cam'] = []
-    images[defalut_cam]['img_path'] = ori_info['image_path'].split('/')[-1]
-    img_path = os.path.join(root_path, 'image', images[defalut_cam]['img_path'])
+    images[defalut_cam]['img_path'] = ori_info[sensor_view + '_image_path']
+    img_path = os.path.join(root_path, images[defalut_cam]['img_path'])
     image_shape = np.array(io.imread(img_path).shape[:2], dtype=np.int32)
     images[defalut_cam]['height'] = image_shape[0]
     images[defalut_cam]['width'] = image_shape[1]
 
-    sensor_view = args.dataset
-    path_camera_intrinsic = os.path.join(root_path, 'calib/camera_intrinsic')
-    if sensor_view == 'vehicle-side' or sensor_view == 'cooperative':
-        path_lidar_to_camera = os.path.join(root_path, 'calib/lidar_to_camera')
-    else:
-        path_lidar_to_camera = os.path.join(root_path, 'calib/virtuallidar_to_camera')
-    camera_intrisinc_path = os.path.join(path_camera_intrinsic, images[defalut_cam]['img_path'].replace('.jpg', '.json'))
-    cam_D, cam_K = get_cam_D_and_cam_K(camera_intrisinc_path)
-    lidar_to_camera_path = os.path.join(path_lidar_to_camera, images[defalut_cam]['img_path'].replace('.jpg', '.json'))
-    t_velo2cam, r_velo2cam = get_velo2cam(lidar_to_camera_path)
+    calib_v_lidar2cam_filename = os.path.join(root_path, 'vehicle-side/calib/lidar_to_camera', veh_idx + '.json')
+    calib_v_lidar2cam = load(calib_v_lidar2cam_filename)
+    calib_v_cam_intrinsic_filename = os.path.join(root_path, 'vehicle-side/calib/camera_intrinsic/', veh_idx + '.json')
+    calib_v_cam_intrinsic = load(calib_v_cam_intrinsic_filename)
+    R0_rect = np.identity(4)
+    Tr_velo_to_cam = np.identity(4)
+    Tr_velo_to_cam[0:3, 0:3] = calib_v_lidar2cam['rotation']
+    Tr_velo_to_cam[0:3, 3] = [calib_v_lidar2cam['translation'][0][0], calib_v_lidar2cam['translation'][1][0], calib_v_lidar2cam['translation'][2][0]]
+    P2 = np.identity(4)
+    P2[0:3, 0:3] = np.array(calib_v_cam_intrinsic['cam_K']).reshape(3, 3)
+    lidar2cam = R0_rect @ Tr_velo_to_cam
 
-    t_velo2cam = np.array(t_velo2cam).reshape(3, 1)
-    r_velo2cam = np.array(r_velo2cam).reshape(3, 3)
-    P2, Tr_velo_to_cam = convert_calib_v2x_to_kitti(cam_D, cam_K, t_velo2cam, r_velo2cam)
-    P2 = _extend_matrix(np.array(P2).reshape(3, 4))
-    Tr_velo_to_cam = _extend_matrix(np.array(Tr_velo_to_cam).reshape(3, 4))
     images[defalut_cam]['cam2img'] = P2
     images[defalut_cam]['tr_velo_to_cam'] = Tr_velo_to_cam
-    images[defalut_cam]['t_velo2cam'] = t_velo2cam
-    images[defalut_cam]['r_velo2cam'] = r_velo2cam
-
-    R0_rect = np.array([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])
-    rect_4x4 = np.zeros([4, 4], dtype=R0_rect.dtype)
-    rect_4x4[3, 3] = 1.
-    rect_4x4[:3, :3] = R0_rect
-    R0_rect = rect_4x4
-
+    images[defalut_cam]['t_velo2cam'] = calib_v_lidar2cam['translation']
+    images[defalut_cam]['r_velo2cam'] = calib_v_lidar2cam['rotation']
     images['R0_rect'] = R0_rect
-    lidar2cam = R0_rect @ Tr_velo_to_cam
     images[defalut_cam]['lidar2cam'] = lidar2cam
     images[defalut_cam]['lidar2img'] = P2 @ lidar2cam
 
@@ -99,95 +85,97 @@ def get_cam_instances(images, metainfo, root_path):
     for cam_idx in range(4):
         cam_instances['CAM%d' % cam_idx] = []
 
-    cam_labels_path = os.path.join(root_path, 'label/camera', images[defalut_cam]['img_path'].replace('.jpg', '.json'))
-    label_infos = load(cam_labels_path)
-    if label_infos == []:
-        return cam_instances
-    for label_info in label_infos:
-        cam_instance = {}
-        cam_instance['bbox'] = [label_info['2d_box']['xmin'], label_info['2d_box']['ymin'], label_info['2d_box']['xmax'], label_info['2d_box']['ymax']]
-        if label_info['type'] in ['Truck', 'Van', 'Bus']:
-            label_info['type'] == 'Car'
+    # cam_labels_path = os.path.join(root_path, 'label/camera', images[defalut_cam]['img_path'].replace('.jpg', '.json'))
+    # label_infos = load(cam_labels_path)
+    # if label_infos == []:
+    #     return cam_instances
+    # for label_info in label_infos:
+    #     cam_instance = {}
+    #     cam_instance['bbox'] = [label_info['2d_box']['xmin'], label_info['2d_box']['ymin'], label_info['2d_box']['xmax'], label_info['2d_box']['ymax']]
+    #     if label_info['type'] in ['Truck', 'Van', 'Bus']:
+    #         label_info['type'] == 'Car'
 
-        if label_info['type'] in metainfo['classes']:
-            cam_instance['bbox_label'] = metainfo['classes'].index(label_info['type'])
-            cam_instance['bbox_label_3d'] = cam_instance['bbox_label']
-        else:
-            cam_instance['bbox_label'] = -1
-            cam_instance['bbox_label_3d'] = -1
-            continue
+    #     if label_info['type'] in metainfo['classes']:
+    #         cam_instance['bbox_label'] = metainfo['classes'].index(label_info['type'])
+    #         cam_instance['bbox_label_3d'] = cam_instance['bbox_label']
+    #     else:
+    #         cam_instance['bbox_label'] = -1
+    #         cam_instance['bbox_label_3d'] = -1
+    #         continue
 
-        h, w, l, x, y, z, yaw_lidar = get_label(label_info)
-        z = z - h / 2
-        bottom_center = [x, y, z]
-        obj_size = [l, w, h]
+    #     h, w, l, x, y, z, yaw_lidar = get_label(label_info)
+    #     z = z - h / 2
+    #     bottom_center = [x, y, z]
+    #     obj_size = [l, w, h]
 
-        bottom_center_in_cam = images[defalut_cam]['r_velo2cam'] * np.matrix(bottom_center).T + images[defalut_cam]['t_velo2cam']
-        alpha, yaw = get_camera_3d_8points(obj_size, yaw_lidar, bottom_center, bottom_center_in_cam, images[defalut_cam]['r_velo2cam'], images[defalut_cam]['t_velo2cam'])
-        [cam_x, cam_y, cam_z, _] = convert_point(np.array([x, y, z, 1]).T, images[defalut_cam]['tr_velo_to_cam'])
+    #     bottom_center_in_cam = images[defalut_cam]['r_velo2cam'] * np.matrix(bottom_center).T + images[defalut_cam]['t_velo2cam']
+    #     alpha, yaw = get_camera_3d_8points(obj_size, yaw_lidar, bottom_center, bottom_center_in_cam, images[defalut_cam]['r_velo2cam'], images[defalut_cam]['t_velo2cam'])
+    #     [cam_x, cam_y, cam_z, _] = convert_point(np.array([x, y, z, 1]).T, images[defalut_cam]['tr_velo_to_cam'])
 
-        cam_instance['bbox_3d'] = [cam_x, cam_y, cam_z, l, w, h, yaw]
-        cam_instance['velocity'] = -1
+    #     cam_instance['bbox_3d'] = [cam_x, cam_y, cam_z, l, w, h, yaw]
+    #     cam_instance['velocity'] = -1
 
-        loc = np.array([cam_x, cam_y, cam_z])
-        dims = np.array([l, w, h])
+    #     loc = np.array([cam_x, cam_y, cam_z])
+    #     dims = np.array([l, w, h])
 
-        dst = np.array([0.5, 0.5, 0.5])
-        src = np.array([0.5, 1.0, 0.5])
+    #     dst = np.array([0.5, 0.5, 0.5])
+    #     src = np.array([0.5, 1.0, 0.5])
 
-        center_3d = loc + dims * (dst - src)
-        center_2d = points_cam2img(center_3d.reshape([1, 3]), images[defalut_cam]['cam2img'], with_depth=True)
-        center_2d = center_2d.squeeze().tolist()
+    #     center_3d = loc + dims * (dst - src)
+    #     center_2d = points_cam2img(center_3d.reshape([1, 3]), images[defalut_cam]['cam2img'], with_depth=True)
+    #     center_2d = center_2d.squeeze().tolist()
 
-        cam_instance['center_2d'] = center_2d[:2]
-        cam_instance['depth'] = center_2d[2]
+    #     cam_instance['center_2d'] = center_2d[:2]
+    #     cam_instance['depth'] = center_2d[2]
 
-        # loc_center = loc + dims * (dst - src)
-        # gt_bbox_3d = np.concatenate([loc_center, dims, [yaw]]).astype(np.float32)
-        # corners_3d = box_np_ops.center_to_corner_box3d(gt_bbox_3d[:, :3], gt_bbox_3d[:, 3:6], gt_bbox_3d[:, 6], (0.5, 0.5, 0.5), axis=1)
-        # corners_3d = corners_3d[0].T  # (1, 8, 3) -> (3, 8)
-        # in_front = np.argwhere(corners_3d[2, :] > 0).flatten()
-        # corners_3d = corners_3d[:, in_front]
+    #     # loc_center = loc + dims * (dst - src)
+    #     # gt_bbox_3d = np.concatenate([loc_center, dims, [yaw]]).astype(np.float32)
+    #     # corners_3d = box_np_ops.center_to_corner_box3d(gt_bbox_3d[:, :3], gt_bbox_3d[:, 3:6], gt_bbox_3d[:, 6], (0.5, 0.5, 0.5), axis=1)
+    #     # corners_3d = corners_3d[0].T  # (1, 8, 3) -> (3, 8)
+    #     # in_front = np.argwhere(corners_3d[2, :] > 0).flatten()
+    #     # corners_3d = corners_3d[:, in_front]
 
-        # # Project 3d box to 2d.
-        # camera_intrinsic = data_info['images']["CAM%d"]['cam2img']
-        # corner_coords = view_points(corners_3d, camera_intrinsic, True).T[:, :2].tolist()
+    #     # # Project 3d box to 2d.
+    #     # camera_intrinsic = data_info['images']["CAM%d"]['cam2img']
+    #     # corner_coords = view_points(corners_3d, camera_intrinsic, True).T[:, :2].tolist()
 
-        # # Keep only corners that fall within the image.
-        # final_coords = post_process_coords(corner_coords, imsize=(data_info['images'][defalut_cam]['width'], data_info['images'][defalut_cam]['height']))
+    #     # # Keep only corners that fall within the image.
+    #     # final_coords = post_process_coords(corner_coords, imsize=(data_info['images'][defalut_cam]['width'], data_info['images'][defalut_cam]['height']))
 
-        # # Skip if the convex hull of the re-projected corners
-        # # does not intersect the image canvas.
-        # if final_coords is None:
-        #     continue
-        # else:
-        #     min_x, min_y, max_x, max_y = final_coords
+    #     # # Skip if the convex hull of the re-projected corners
+    #     # # does not intersect the image canvas.
+    #     # if final_coords is None:
+    #     #     continue
+    #     # else:
+    #     #     min_x, min_y, max_x, max_y = final_coords
 
-        # # Generate dictionary record to be included in the .json file.
-        # cam_instance['bbox'] = [min_x, min_y, max_x, max_y]
-        # cam_instance['bbox_3d_isvalid'] = True
+    #     # # Generate dictionary record to be included in the .json file.
+    #     # cam_instance['bbox'] = [min_x, min_y, max_x, max_y]
+    #     # cam_instance['bbox_3d_isvalid'] = True
 
-        # # If mono3d=True, add 3D annotations in camera coordinates
-        # # use bottom center to represent the bbox_3d
-        # cam_instance['bbox_3d'] = np.concatenate([loc, dims, yaw], axis=1).astype(np.float32).squeeze().tolist()
-        # cam_instance['velocity'] = -1  # no velocity in KITTI
+    #     # # If mono3d=True, add 3D annotations in camera coordinates
+    #     # # use bottom center to represent the bbox_3d
+    #     # cam_instance['bbox_3d'] = np.concatenate([loc, dims, yaw], axis=1).astype(np.float32).squeeze().tolist()
+    #     # cam_instance['velocity'] = -1  # no velocity in KITTI
 
-        # center_3d = np.array(loc_center).reshape([1, 3])
-        # center_2d_with_depth = points_cam2img(
-        #     center_3d, camera_intrinsic, with_depth=True)
-        # center_2d_with_depth = center_2d_with_depth.squeeze().tolist()
+    #     # center_3d = np.array(loc_center).reshape([1, 3])
+    #     # center_2d_with_depth = points_cam2img(
+    #     #     center_3d, camera_intrinsic, with_depth=True)
+    #     # center_2d_with_depth = center_2d_with_depth.squeeze().tolist()
 
-        # cam_instance['center_2d'] = center_2d_with_depth[:2]
-        # cam_instance['depth'] = center_2d_with_depth[2]
+    #     # cam_instance['center_2d'] = center_2d_with_depth[:2]
+    #     # cam_instance['depth'] = center_2d_with_depth[2]
 
-        cam_instances[defalut_cam].append(cam_instance)
+    #     cam_instances[defalut_cam].append(cam_instance)
     return cam_instances
 
 
 def get_instances(images, metainfo, root_path):
     instances = []
 
-    lidar_labels_path = os.path.join(root_path, 'label/lidar', images[defalut_cam]['img_path'].replace('.jpg', '.json'))
+    # TODO 直接从label_world中读取
+    lidar_labels_path = os.path.join(root_path, 'cooperative/label/lidar', images[defalut_cam]['img_path'].replace('.jpg', '.json').split('/')[-1])
+
     label_infos = load(lidar_labels_path)
     if label_infos == []:
         return instances
@@ -197,8 +185,8 @@ def get_instances(images, metainfo, root_path):
         instance = get_empty_instance()
 
         instance['bbox'] = [label_info['2d_box']['xmin'], label_info['2d_box']['ymin'], label_info['2d_box']['xmax'], label_info['2d_box']['ymax']]
-
-        if label_info['type'] in ['Truck', 'Van', 'Bus']:
+        #
+        if label_info['type'] in ['truck', 'van', 'bus', 'car']:
             label_info['type'] = 'Car'
 
         if label_info['type'] in metainfo['classes']:
@@ -245,7 +233,7 @@ def get_instances(images, metainfo, root_path):
         instance['difficulty'] = 2
         object_idx += 1
 
-        v_path = os.path.join(root_path, 'velodyne', lidar_points['lidar_path'])
+        v_path = os.path.join(root_path, lidar_points['lidar_path'])
         points_v = np.fromfile(v_path, dtype=np.float32, count=-1).reshape([-1, lidar_points['num_pts_feats']])
         rect = images['R0_rect']
         Trv2c = lidar_points['Tr_velo_to_cam']
@@ -267,7 +255,7 @@ def get_instances(images, metainfo, root_path):
 
 
 if __name__ == '__main__':
-    root_path = os.path.join(args.root_path, args.dataset)
+    root_path = args.root_path
     dair_infos_trainval_path = os.path.join(root_path, 'dair_infos_trainval.pkl')
     dair_infos_train_path = os.path.join(root_path, 'dair_infos_train.pkl')
     dair_infos_val_path = os.path.join(root_path, 'dair_infos_val.pkl')
@@ -288,19 +276,32 @@ if __name__ == '__main__':
     data_infos_val['metainfo'] = metainfo
     data_infos_val['data_list'] = []
 
-    ori_dair_infos_train = load(os.path.join(root_path, 'data_info.json'))
+    ori_dair_infos_train = load(os.path.join(root_path, args.dataset, 'data_info.json'))
     defalut_cam = 'CAM2'
     sample_idx = 0
     sample_idx_train = 0
     sample_idx_val = 0
     for ori_info in tqdm(ori_dair_infos_train):
-        data_info = {'sample_idx': 0, 'images': {}, 'lidar_points': {}, 'instances': {}, 'cam_instances': {}}
+        data_info = {
+            'sample_idx': 0,
+            'veh_sample_idx': 0,
+            'inf_sample_idx': 0,
+            'calib': {},
+            'images': {},
+            'lidar_points': {},
+            'inf_images': {},
+            'inf_lidar_points': {},
+            'instances': {},
+            'cam_instances': {}
+        }
+        data_info['veh_sample_idx'] = ori_info['vehicle_image_path'].split('/')[-1].replace('.jpg', '')
+        data_info['inf_sample_idx'] = ori_info['infrastructure_image_path'].split('/')[-1].replace('.jpg', '')
 
-        images = get_images_info(ori_info, defalut_cam, root_path)
+        images = get_images_info(ori_info, defalut_cam, root_path, data_info['veh_sample_idx'], data_info['inf_sample_idx'])
 
         lidar_points = {}
         lidar_points['num_pts_feats'] = 4  # default value
-        lidar_points['lidar_path'] = images[defalut_cam]['img_path'].replace('.jpg', '.bin')
+        lidar_points['lidar_path'] = os.path.join('vehicle-side/velodyne', images[defalut_cam]['img_path'].split('/')[-1].replace('.jpg', '.bin'))
         lidar_points['Tr_velo_to_cam'] = images[defalut_cam]['tr_velo_to_cam'].tolist()
         lidar_points['Tr_imu_to_velo'] = images[defalut_cam]['tr_velo_to_cam'].tolist()  # equal to Tr_velo_to_cam
 
