009c3084c9e9f45ade9ed771627d48e10c367226
Li Bin <lbin@outlook.com>
2023-11-20 18:18:38 +0800

    add bevfusion

--- setup.py
+++ setup.py
@@ -41,19 +41,23 @@ def make_cuda_ext(name, module, sources, sources_cuda=[], extra_args=[], extra_i
             '-D__CUDA_NO_HALF_OPERATORS__',
             '-D__CUDA_NO_HALF_CONVERSIONS__',
             '-D__CUDA_NO_HALF2_OPERATORS__',
+            '-gencode=arch=compute_70,code=sm_70',
+            '-gencode=arch=compute_75,code=sm_75',
+            '-gencode=arch=compute_80,code=sm_80',
+            '-gencode=arch=compute_86,code=sm_86',
         ]
         sources += sources_cuda
     else:
         print('Compiling {} without CUDA'.format(name))
         extension = CppExtension
-        # raise EnvironmentError('CUDA is required to compile MMDetection!')
 
     return extension(
         name='{}.{}'.format(module, name),
         sources=[os.path.join(*module.split('.'), p) for p in sources],
         include_dirs=extra_include_path,
         define_macros=define_macros,
-        extra_compile_args=extra_compile_args)
+        extra_compile_args=extra_compile_args,
+    )
 
 
 def parse_requirements(fname='requirements.txt', with_version=True):
@@ -161,6 +165,25 @@ if __name__ == '__main__':
                     'src/voxelization_cpu.cpp',
                     'src/voxelization_cuda.cu',
                 ]),
+            make_cuda_ext(
+                name='bev_pool_ext',
+                module='transvision.bevfusion.ops.bev_pool',
+                sources=[
+                    'src/bev_pool.cpp',
+                    'src/bev_pool_cuda.cu',
+                ],
+            ),
+            make_cuda_ext(
+                name='voxel_layer',
+                module='transvision.bevfusion.ops.voxel',
+                sources=[
+                    'src/voxelization.cpp',
+                    'src/scatter_points_cpu.cpp',
+                    'src/scatter_points_cuda.cu',
+                    'src/voxelization_cpu.cpp',
+                    'src/voxelization_cuda.cu',
+                ],
+            ),
         ],
         cmdclass={'build_ext': BuildExtension},
         zip_safe=False)
--- transvision/version.py
+++ transvision/version.py
@@ -1,2 +1,2 @@
 __version__ = '0.1.0'
-git_version = 'ffa9653f6eeb3d202cc4e5cb21bc0a3e374c2b88'
+git_version = '8d93e204898c036baeb69e28191ee4a5a13a4b79'
