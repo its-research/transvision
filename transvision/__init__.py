88b8972cad65998028c16f0a96784a30b4c31c98
Li Bin <lbin@outlook.com>
2023-11-30 13:42:18 +0800

    update code arch

--- transvision/__init__.py
+++ transvision/__init__.py
@@ -2,7 +2,7 @@ from transvision.dataset.formating import Pack3DDetDAIRInputs
 from transvision.dataset.loading import LoadPointsFromFile_w_sensor_view
 from transvision.dataset.v2x_dataset import V2XDataset
 from transvision.evaluation.metrics.dair_v2x_metric import DAIRV2XMetric
-from transvision.models.coformer import CoFormerNet
 from transvision.models.data_preprocessors.data_preprocessor import Det3DDataDAIRPreprocessor
-from transvision.models.feature_flownet import FeatureFlowNet
-from transvision.models.v2x_voxelnet import V2XVoxelNet
+from transvision.models.detectors.coformer import CoFormerNet
+from transvision.models.detectors.feature_flownet import FeatureFlowNet
+from transvision.models.detectors.v2x_voxelnet import V2XVoxelNet
