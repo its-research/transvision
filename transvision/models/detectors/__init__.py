719942b5405601cb60c3f881f0fd1ecfb1105090
Li Bin <lbin@users.noreply.github.com>
2026-01-26 15:25:40 +0800

    Add resilient V2X detector components (#3)

--- transvision/models/detectors/__init__.py
+++ transvision/models/detectors/__init__.py
@@ -3,6 +3,7 @@ from .bevformer_fp16 import BEVFormer_fp16
 from .bevformerV2 import BEVFormerV2
 from .coformernet import CoFormerNet
 from .feature_flownet import FeatureFlowNet
+from .resilient_v2x import ResilientV2XNet
 from .v2x_voxelnet import V2XVoxelNet
 
-__all__ = ['BEVFormer', 'BEVFormer_fp16', 'BEVFormerV2', 'CoFormerNet', 'FeatureFlowNet', 'V2XVoxelNet']
+__all__ = ['BEVFormer', 'BEVFormer_fp16', 'BEVFormerV2', 'CoFormerNet', 'FeatureFlowNet', 'ResilientV2XNet', 'V2XVoxelNet']
